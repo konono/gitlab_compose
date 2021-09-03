744128d0f3eeba7f6ed3d39301ab3087596ff78e
Yuki Yamashita <kono@kononet.net>
2021-09-03 14:24:39 +0900

    Update start_firewall.sh

--- start_firewall.sh
+++ start_firewall.sh
@@ -6,7 +6,10 @@ firewall-cmd  --zone=public --add-port=10022/tcp
 firewall-cmd  --zone=public --add-port=80/tcp
 firewall-cmd  --zone=public --add-port=443/tcp
 firewall-cmd  --zone=public --add-port=8080/tcp
+firewall-cmd  --zone=public --add-port=10443/tcp
 firewall-cmd  --zone=public --add-port=10022/tcp --permanent
 firewall-cmd  --zone=public --add-port=80/tcp --permanent
 firewall-cmd  --zone=public --add-port=443/tcp --permanent
 firewall-cmd  --zone=public --add-port=8080/tcp --permanent
+firewall-cmd  --zone=public --add-port=10443/tcp --permanent
+
